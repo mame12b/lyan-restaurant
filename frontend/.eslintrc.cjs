9313658d1bf9cda04e26470bc4e8cf3007388254
mame12b <mame4005@gmail.com>
2025-11-05 21:51:08 +0400

    update front-end and back-end

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,6 +1,36 @@
 module.exports = {
-  extends: ['react-app', 'react-app/jest'],
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: ['react', 'react-hooks'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   rules: {
-    'react-hooks/exhaustive-deps': 'off'
-  }
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    'react/no-unescaped-entities': 'warn'
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.{js,jsx,ts,tsx}', '**/*.{test,spec}.{js,jsx,ts,tsx}'],
+      env: {
+        jest: true
+      }
+    }
+  ],
+  ignorePatterns: ['coverage/']
 };

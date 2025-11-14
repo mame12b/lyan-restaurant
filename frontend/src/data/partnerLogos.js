e33e2707015bb4ecb981a630f22309145daf96c7
mame12b <mame4005@gmail.com>
2025-11-15 03:35:05 +0400

    modifying partnersLogo.js

--- frontend/src/data/partnerLogos.js
+++ frontend/src/data/partnerLogos.js
@@ -1,32 +1,32 @@
 const partnerLogos = [
   {
     name: 'Ethiopian Airlines',
-    lightLogo: 'https://static.cdnlogo.com/logos/e/1/ethiopian-airlines.svg',
+    lightLogo: 'https://assets.stickpng.com/images/668e5a41847a2841c9ada830.png',
     darkLogo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Ethiopian_Airlines_Logo_2019.svg/640px-Ethiopian_Airlines_Logo_2019.svg.png'
   },
   {
     name: 'Commercial Bank of Ethiopia',
-    lightLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/59/Commercial_Bank_of_Ethiopia_logo.svg/320px-Commercial_Bank_of_Ethiopia_logo.svg.png',
+    lightLogo: 'https://www.advantechafrica.com/wp-content/uploads/2020/01/images.png',
     darkLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/59/Commercial_Bank_of_Ethiopia_logo.svg/640px-Commercial_Bank_of_Ethiopia_logo.svg.png'
   },
   {
     name: 'Ethio Telecom',
-    lightLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/Ethio_Telecom_logo.svg/320px-Ethio_Telecom_logo.svg.png',
+    lightLogo: 'https://ethiotelecom.afrotender.com/staff/assets/images/ethiotelecom-logo.jpg',
     darkLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/Ethio_Telecom_logo.svg/640px-Ethio_Telecom_logo.svg.png'
   },
   {
-    name: 'Dashen Bank',
-    lightLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3a/Dashen_Bank_logo.svg/320px-Dashen_Bank_logo.svg.png',
+    name: 'Tamcon Software Solutions',
+    lightLogo: 'https://www.ethiolottery.et/images/heroImages/etlottery2.svg',
     darkLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3a/Dashen_Bank_logo.svg/640px-Dashen_Bank_logo.svg.png'
   },
   {
     name: 'Sheraton Addis',
-    lightLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/Sheraton_Hotels_and_Resorts_logo.svg/320px-Sheraton_Hotels_and_Resorts_logo.svg.png',
+    lightLogo: 'https://sheraton-addis.hotels-addisababa.com/data/Imgs/1920x1080w/5544/554406/554406663/img-sheraton-addis-a-luxury-collection-hotel-addis-ababa-addis-ababa-1.JPEG',
     darkLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/Sheraton_Hotels_and_Resorts_logo.svg/640px-Sheraton_Hotels_and_Resorts_logo.svg.png'
   },
   {
     name: 'Habesha Breweries',
-    lightLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Habesha_Brewery_logo.svg/320px-Habesha_Brewery_logo.svg.png',
+    lightLogo: 'https://images.squarespace-cdn.com/content/v1/59f0a6e9f09ca487886b21e2/1521809681164-3WG5QQGC2VHRA5IROSCQ/Artboard+1+copy871C+Landscap.png',
     darkLogo: 'https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Habesha_Brewery_logo.svg/640px-Habesha_Brewery_logo.svg.png'
   }
 ];
